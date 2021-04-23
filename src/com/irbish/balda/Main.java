639641efe58ddab3b21e11115af9b53c5b10f530
cgarth116 <victorkyltashev@gmail.com>
2021-04-23 12:11:43 +0300

    add work with dictionary

--- src/com/irbish/balda/Main.java
+++ src/com/irbish/balda/Main.java
@@ -1,5 +1,6 @@
 package com.irbish.balda;
 
+import java.io.*;
 import java.util.*;
 
 public class Main {
@@ -10,10 +11,11 @@ public class Main {
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
     //help metod's
-    public static boolean   validateWord(char[] userWord, LinkedList<Character> lettersForUse){
+    private static boolean  wordCheckByLetterOccurrence(String userWord, LinkedList<Character> lettersForUse){
+        var userWordForLetter = userWord.toCharArray();
         LinkedList<Character> tmp = (LinkedList<Character>) lettersForUse.clone();
 
-        for (Character letter : userWord){
+        for (Character letter : userWordForLetter){
             if(!tmp.contains(letter)){
                 return false;
             }
@@ -21,7 +23,13 @@ public class Main {
         }
         return true;
     }
-    public static int       enterCountPlayers(int countPlayers){
+    private static boolean  checkInProgramDictionary(String userWord, HashMap dictionary) {
+        if (!dictionary.containsKey(userWord)) {
+            return false;
+        }
+        return true;
+    }
+    private static int      enterCountPlayers(int countPlayers){
         var in = new Scanner(System.in);
         while (countPlayers == 0) {
             System.out.print("Введите кол-во игроков(1-10): ");
@@ -32,12 +40,12 @@ public class Main {
         }
         return countPlayers;
     }
-    public static String    enterWord(String message){
+    private static String   enterWord(String message){
         var in = new Scanner(System.in);
         System.out.print(message);
         return in.nextLine();
     }
-    public static void      resultGame(Map<Integer, LinkedHashSet<String>> wordHistoryAll){
+    private static void     resultGame(Map<Integer, LinkedHashSet<String>> wordHistoryAll){
         Queue<Integer> whoWin = new LinkedList();                                   //list of winners
         var y = wordHistoryAll.get(1).size();
         var countPlayers = wordHistoryAll.size();
@@ -62,35 +70,82 @@ public class Main {
             System.out.println(ANSI_PURPLE + "Поздравляем игрока №" + whoWin.poll() + " с победой!!!" + ANSI_RESET);
         }
     }
+    private static HashMap  addWordInDictionary(BufferedReader reader, HashMap dictionary) throws IOException {
+        try{
+            String str = reader.readLine();
+            while (str != null) {
+                dictionary.put(str, 0);
+                str = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new IOException(e.getMessage());
+        }
+        return dictionary;
+    }
+    private static HashMap  loadDictionary() throws IOException {
+        var dictionary = new HashMap<String, Integer>();
+        File dictionaryRus = new File("./src/com/irbish/balda/russianDictionary.txt");
+        File dictionaryEng = new File("./src/com/irbish/balda/englishDictionary.txt");
 
-    public static void main(String[] args) {
+        try {
+            //add russian dictionary
+            FileReader frRus = new FileReader(dictionaryRus);
+            BufferedReader readerRus = new BufferedReader(frRus);
+            dictionary = addWordInDictionary(readerRus, dictionary);
+
+            //add english dictionary
+            FileReader frEng = new FileReader(dictionaryEng);
+            BufferedReader readerEng = new BufferedReader(frEng);
+            dictionary = addWordInDictionary(readerEng, dictionary);
+        } catch (IOException e) {
+            throw new IOException(e.getMessage());
+        }
+        return dictionary;
+    }
+    private static void     fillingLettersToCompose(LinkedList<Character> lettersForUse, String inputWord){
+        for(char ch : inputWord.toCharArray()){
+            lettersForUse.add(ch);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         int y = 0;
         var lettersForUse = new LinkedList<Character>();                            //letters to compose word's
         var wordHistoryAll = new HashMap<Integer, LinkedHashSet<String>>();         //dictionary of entered words
+        var dictionary = new HashMap<String, Integer>();                            //rus-eng dictionary
         Queue<Integer> playerQueue = new LinkedList();                              //queue for players
 
+        //add rus-eng dictionary
+        try {
+            dictionary = loadDictionary();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        //input initial conditions
         var countPlayers = enterCountPlayers(0);
         var inputWord = enterWord("Введите начальное слово:");
 
-        for(int i = 1; i <= countPlayers; i++){                                     //create new players and history dic
+        for(int i = 1; i <= countPlayers; i++){                                     //create queue players, history dic
             playerQueue.offer(i);
             wordHistoryAll.put(i, new LinkedHashSet());
         }
         Integer whoseMove = playerQueue.poll();                                     //get first player
 
-        for(char ch : inputWord.toCharArray()){                                     //filling letters to compose word's
-            lettersForUse.add(ch);
-        }
+        fillingLettersToCompose(lettersForUse, inputWord);                          //filling letters to compose word's
 
         //let's play now
         while(whoseMove != null) {
             var userWord = enterWord("\nХод игрока №" + whoseMove + ":");
 
             while (!userWord.isEmpty() || !userWord.isBlank()) {
-                if (!validateWord(userWord.toCharArray(), lettersForUse)) {
-                    System.out.println(ANSI_RED + "Слово \"" + userWord + "\" не подходит" + ANSI_RESET);
-                    userWord = enterWord(ANSI_YELLOW + "Повторный ход игрока №" + whoseMove + ":" + ANSI_RESET);
-                    continue;
+                if (!wordCheckByLetterOccurrence(userWord, lettersForUse) ||
+                        !checkInProgramDictionary(userWord, dictionary)) {
+                            System.out.println(ANSI_RED + "Слово \"" + userWord +
+                                                "\" не подходит\\не найдено в словаре программы" + ANSI_RESET);
+                            userWord = enterWord(ANSI_YELLOW + "Повторный ход игрока №" + whoseMove + ":" + ANSI_RESET);
+                        continue;
                 }
 
                 y = 1;
@@ -106,11 +161,8 @@ public class Main {
                     continue;
                 }
                 wordHistoryAll.get(whoseMove).add(userWord);
-                System.out.println("Игрок №" +
-                                    whoseMove +
-                                    " : Счет увеличен на +1(Итого: " +
-                                    wordHistoryAll.get(whoseMove).size() +
-                                    ")\n");
+                System.out.println("Игрок №" + whoseMove + " : Счет увеличен на +1(Итого: " +
+                                    wordHistoryAll.get(whoseMove).size() + ")\n");
                 playerQueue.offer(whoseMove);                                       //put player in queue for next move
                 whoseMove = playerQueue.poll();                                     //get player from queue who must move
 
